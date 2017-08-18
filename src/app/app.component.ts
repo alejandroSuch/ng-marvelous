984850aaad0882ad6c3a507a6ed955808536df99
Alejandro Such Berenguer <alejandro.such@gmail.com>
2017-08-18 13:14:01 +0200

    Playing with template variables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,21 +8,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'marvel';
 
-  helloWorldImgSrc = 'https://i.ytimg.com/vi/zecueq-mo4M/maxresdefault.jpg';
-  bgColor = 'red';
-  useRedBgColor = false;
-  buttonDisabled = true;
-  inputValue = 'initial value';
-
-  toggleBtn() {
-    this.buttonDisabled = !this.buttonDisabled;
-  }
-
-  onInputChanged(value) {
-    this.inputValue = value;
-  }
-
-  clearInputValue() {
-    this.inputValue = '';
+  call(number) {
+    alert(`Calling ${number}...`);
   }
 }
