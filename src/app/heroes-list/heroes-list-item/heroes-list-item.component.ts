5c0a9d4852be53911167d7e4cf96aafa6f2c99c7
Alejandro Such Berenguer <alejandro.such@gmail.com>
2017-08-21 16:51:17 +0200

    Finally using native view encapsulation

--- src/app/heroes-list/heroes-list-item/heroes-list-item.component.ts
+++ src/app/heroes-list/heroes-list-item/heroes-list-item.component.ts
@@ -1,9 +1,10 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnInit, ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'marvel-heroes-list-item',
   templateUrl: './heroes-list-item.component.html',
-  styleUrls: ['./heroes-list-item.component.less']
+  styleUrls: ['./heroes-list-item.component.less'],
+  encapsulation: ViewEncapsulation.Native
 })
 export class HeroesListItemComponent implements OnInit {
   @Input()
