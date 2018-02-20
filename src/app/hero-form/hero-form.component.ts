f9500d7520e08009f232b0d29dc40627a4952252
Alejandro Such Berenguer <alejandro.such@gmail.com>
2018-02-20 12:41:58 +0100

    Did some DOM manipulations

--- src/app/hero-form/hero-form.component.ts
+++ src/app/hero-form/hero-form.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {Component, ElementRef, EventEmitter, OnInit, Output, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'marvel-hero-form',
@@ -9,14 +9,20 @@ export class HeroFormComponent implements OnInit {
   @Output()
   onHeroAdded: EventEmitter<any> = new EventEmitter<any>();
 
+  @ViewChild('heroName') heroName: ElementRef;
+  @ViewChild('hasTights') hasTights: ElementRef;
+
   constructor() {
   }
 
   ngOnInit() {
   }
 
-  addHero(name, hasTights = false) {
+  addHero(name, hasTights) {
     this.onHeroAdded.emit({name, hasTights});
+
+    this.heroName.nativeElement.value = '';
+    this.hasTights.nativeElement.checked = false;
   }
 
 }
