82b1dae623546a448c57c2a1fb5bb866224390e7
Alejandro Such Berenguer <alejandro.such@gmail.com>
2018-04-04 00:57:04 +0200

    Now I can list planets

--- src/app/planets/components/list/list.component.ts
+++ src/app/planets/components/list/list.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { PlanetRepositoryService } from './../../planet-repository.service';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Planet } from '../../planet';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'sw-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.less']
 })
-export class ListComponent implements OnInit {
-
-  constructor() { }
+export class ListComponent implements OnInit, OnDestroy {
+  private planets: Planet[];
+  private subscription: Subscription;
+  constructor(private planetRepository: PlanetRepositoryService) { }
 
   ngOnInit() {
+    this.subscription = this.planetRepository.getPage(1).subscribe(
+      (planets: Planet[]) => {
+        this.planets = planets
+      }
+    );
   }
 
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 }
