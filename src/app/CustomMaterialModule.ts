54b06ec6288604c9d02bb4e0bc71dc3895401d03
Alejandro Such Berenguer <alejandro.such@gmail.com>
2018-04-09 23:23:23 +0200

    Add loading spinner

--- src/app/CustomMaterialModule.ts
+++ src/app/CustomMaterialModule.ts
@@ -5,11 +5,12 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 
 @NgModule({
-  imports: [ MatToolbarModule, MatIconModule, MatTooltipModule, MatButtonModule, MatCardModule ],
-  exports: [ MatToolbarModule, MatIconModule, MatTooltipModule, MatButtonModule, MatCardModule ],
+  imports: [ MatToolbarModule, MatIconModule, MatTooltipModule, MatButtonModule, MatCardModule, MatProgressSpinnerModule ],
+  exports: [ MatToolbarModule, MatIconModule, MatTooltipModule, MatButtonModule, MatCardModule, MatProgressSpinnerModule ],
 })
 export class CustomMaterialModule{
 
