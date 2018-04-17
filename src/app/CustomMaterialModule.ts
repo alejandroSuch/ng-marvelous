a353c89c4c3b00afa9fe2c3e11017cb5cb2db0fa
Alejandro Such Berenguer <alejandro.such@gmail.com>
2018-04-17 21:56:58 +0200

    Pagination

--- src/app/CustomMaterialModule.ts
+++ src/app/CustomMaterialModule.ts
@@ -6,11 +6,12 @@ import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatPaginatorModule } from '@angular/material/paginator';
 
 
 @NgModule({
-  imports: [ MatToolbarModule, MatIconModule, MatTooltipModule, MatButtonModule, MatCardModule, MatProgressSpinnerModule ],
-  exports: [ MatToolbarModule, MatIconModule, MatTooltipModule, MatButtonModule, MatCardModule, MatProgressSpinnerModule ],
+  imports: [ MatToolbarModule, MatIconModule, MatTooltipModule, MatButtonModule, MatCardModule, MatProgressSpinnerModule, MatPaginatorModule ],
+  exports: [ MatToolbarModule, MatIconModule, MatTooltipModule, MatButtonModule, MatCardModule, MatProgressSpinnerModule, MatPaginatorModule ],
 })
 export class CustomMaterialModule{
 
--- src/app/planets/list/list.component.ts
+++ src/app/planets/list/list.component.ts
@@ -1,7 +1,14 @@
 import { PlanetRepositoryService } from '../planet-repository.service';
 import { Planet } from './../planet';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit, OnDestroy } from '@angular/core';
 import { Observable, Subscription } from 'rxjs';
+import { MatPaginator, PageEvent } from '@angular/material/paginator';
+import { ActivatedRoute, Router, NavigationExtras, RouterEvent, NavigationStart, NavigationEnd } from '@angular/router';
+
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/delay'
 
 @Component({
   selector: 'sw-list',
@@ -10,10 +17,44 @@ import { Observable, Subscription } from 'rxjs';
 })
 export class ListComponent implements OnInit {
   public planets$: Observable<Planet[]>;
+  public planets:Planet[] = null;
+  public count$: Observable<number>;
+  public currentPage: number = 0;
+  public loaded: boolean;
 
-  constructor(private planetRepository: PlanetRepositoryService) { }
+  constructor(
+    private planetRepository: PlanetRepositoryService,
+    private route: ActivatedRoute,
+    private router: Router,
+  ) { 
+  }
 
   ngOnInit() {
-    this.planets$ = this.planetRepository.getPage(1);
+    this.count$ = this.planetRepository.count();
+
+    const getPageNumber = paramMap => {
+      if(paramMap.has('page')) {
+        return +paramMap.get('page');
+      } else {
+        return 1;
+      }
+    };
+
+    this.route.queryParamMap
+      .do(() => this.loaded = false)
+      .map(getPageNumber)
+      .do(page => this.currentPage = page - 1)
+      .switchMap(page => this.planetRepository.getPage(page))
+      .map(planets => this.planets = planets)
+      .do(() => this.loaded = true)
+      .subscribe();
+  }
+
+  onPageChanged({ pageIndex }: PageEvent) { 
+    const extras: NavigationExtras = {
+      queryParams: { page: pageIndex + 1 }
+    };
+
+    this.router.navigate([], extras);
   }
 }
